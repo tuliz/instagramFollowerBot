70d868b6457fc45c0d8f350d31c987f77b23f23a
yarden <yarden.tulchinsky@gmail.com>
2024-06-27 13:54:00 +0300

    after the login function, created a find_followers function. goes to a specific account page and opens its followers window. starts to click on the follow button and scrolling to the bottom of the window to reload more and keep following

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from instafollower import InstaFollower
 
-EMAIL ='infinitepath.automation'
-PASSWORD = 'Gy!@1924'
-followers_account = 'eran_brownstain'
+EMAIL =''
+PASSWORD = ''
+followers_account = ''
 insta_bot = InstaFollower()
 # Open the instagram page and login to account
 insta_bot.instagram_login(PASSWORD,EMAIL)
