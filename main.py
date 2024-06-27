629e2e1cf43f0f1fcca18ade220ef79b5c7b2f83
yarden <yarden.tulchinsky@gmail.com>
2024-06-27 14:01:14 +0300

    .

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ insta_bot = InstaFollower()
 # Open the instagram page and login to account
 insta_bot.instagram_login(PASSWORD,EMAIL)
 
-# Go to a specific Account in instagram and find his followers and follow them
+#  Go to a specific Account in instagram and find his followers and follow them
 insta_bot.find_followers(followers_account)
